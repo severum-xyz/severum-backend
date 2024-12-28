de06bd68be9df7d938d74f888b30ab2f37f1387e
Antoine James <mushow@proton.me>
2024-12-28 16:55:47 +0100

    refactor: Modularize main function by creating init and run_server functions

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,31 @@ mod controllers;
 mod routes;
 
 use axum::{serve, Router};
+use env_logger::init as log_init;
 use log::info;
 use tokio::net::TcpListener;
 use crate::utils::create_db_pool;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    log_init();
 
+    init().await;
+    run_server().await;
+}
+
+async fn init() {
     dotenv::dotenv().ok();
+    create_db_pool().await;
+}
+
+async fn run_server() {
     let app = Router::new().merge(routes::app_routes());
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("Server running on http://0.0.0.0:3000");
 
-    create_db_pool().await; // This will now log database connection attempts
+    create_db_pool().await;
 
     serve(listener, app).await.unwrap();
 }
