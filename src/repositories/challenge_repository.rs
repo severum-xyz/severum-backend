8e376b84a758f847a62b56b0721caebae6c82360
Antoine James <mushow@proton.me>
2024-12-28 14:33:59 +0100

    feat: add challenge repository to load challenges from filesystem

---
+++ src/repositories/challenge_repository.rs
@@ -0,0 +1,6 @@
+use crate::models::challenge::ChallengeMetadata;
+use crate::utils::load_challenges;
+
+pub fn fetch_all_challenges(base_path: &str) -> Vec<ChallengeMetadata> {
+    load_challenges(base_path)
+}
