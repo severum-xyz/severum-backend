59bd1cb72f43af0603ad6aa0f936e45664230322
Antoine James <mushow@proton.me>
2024-12-31 00:01:19 +0100

    feat: add LoginError enum and enhance error handling for RegistrationError

--- src/models/errors.rs
+++ src/models/errors.rs
@@ -1,5 +1,7 @@
+use diesel::result::Error;
+use jsonwebtoken::errors::Error as JwtError;
+use log::error;
 use thiserror::Error;
-use sqlx::Error as SqlxError;
 
 #[derive(Error, Debug)]
 pub enum RegistrationError {
@@ -13,8 +15,32 @@ pub enum RegistrationError {
     InternalError,
 }
 
-impl From<SqlxError> for RegistrationError {
-    fn from(_: SqlxError) -> Self {
+#[derive(Error, Debug)]
+pub enum LoginError {
+    #[error("Email or password is incorrect")]
+    InvalidCredentials,
+
+    #[error("Internal server error")]
+    InternalError,
+}
+
+impl From<Error> for RegistrationError {
+    fn from(e: Error) -> Self {
+        error!("Database error: {:?}", e);
         RegistrationError::InternalError
+    }
+}
+
+impl From<Error> for LoginError {
+    fn from(e: Error) -> Self {
+        error!("Database error: {:?}", e);
+        LoginError::InternalError
+    }
+}
+
+impl From<JwtError> for LoginError {
+    fn from(e: JwtError) -> Self {
+        error!("JWT error: {:?}", e);
+        LoginError::InternalError
     }
 }
