463777393574506f77e83a8a5f2c401a20d5b5f7
Antoine James <mushow@proton.me>
2024-12-31 00:02:17 +0100

    refactor: update User and NewUser structs for Diesel compatibility

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,20 +1,22 @@
-use diesel::prelude::*;
-use serde::{Serialize, Deserialize};
-use chrono::NaiveDateTime;
 use crate::schema::users;
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+use serde::Deserialize;
 
-#[derive(Queryable, Serialize, Deserialize, Debug)]
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i32,
     pub email: String,
     pub pseudo: String,
     pub password_hash: String,
     pub created_at: NaiveDateTime,
-    pub verified: bool,
+    pub verified: Option<bool>,
 }
 
-#[derive(Insertable, Deserialize, Debug)]
-#[table_name = "users"]
+#[derive(Insertable)]
+#[diesel(table_name = users)]
 pub struct NewUser<'a> {
     pub email: &'a str,
     pub pseudo: &'a str,
