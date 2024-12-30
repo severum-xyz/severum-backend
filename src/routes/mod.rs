c0011cd06008b94a6ad11e703c3b7c3de6df789e
Antoine James <mushow@proton.me>
2024-12-31 00:01:29 +0100

    feat: add login route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,11 @@
 use axum::Router;
 use axum::routing::post;
-use crate::controllers::user_controller::register_user;
+use crate::controllers::user_controller::{login_user_handler, register_user};
 
 mod challenge_routes;
 
 pub fn user_routes() -> Router {
     Router::new()
         .route("/register", post(register_user))
+        .route("/login", post(login_user_handler))
 }
