d00c8584f00cede3808315ab7cfb95c4f8aee67c
Antoine James <mushow@proton.me>
2025-01-01 23:48:01 +0100

    feat(routes): add challenge and category routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,22 @@
 use axum::Router;
-use axum::routing::post;
+use axum::routing::{get, post};
+use crate::controllers::category_controller::{get_categories};
+use crate::controllers::challenge_controller::{get_challenges, load_challenges};
 use crate::controllers::user_controller::{login_user_handler, register_user};
-
-mod challenge_routes;
 
 pub fn user_routes() -> Router {
     Router::new()
         .route("/register", post(register_user))
         .route("/login", post(login_user_handler))
+}
+
+pub fn challenge_routes() -> Router {
+    Router::new()
+        .route("/load-challenges", post(load_challenges))
+        .route("/challenges", get(get_challenges))
+}
+
+pub fn category_routes() -> Router {
+    Router::new()
+        .route("/categories", get(get_categories))
 }
