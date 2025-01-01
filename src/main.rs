d2d432b5f334fdbed1f5b99b3e25abe9c6a1f709
Antoine James <mushow@proton.me>
2025-01-01 23:48:58 +0100

    feat(main): add challenge, category population on startup and more routes

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ mod routes;
 pub mod schema;
 
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use axum::{serve, Router};
 use env_logger::init as log_init;
 use log::info;
 use tokio::net::TcpListener;
-use crate::utils::{clone_or_update_repository};
+use crate::services::category_service::CategoryService;
+use crate::utils::{challenge_loader, clone_or_update_repository, get_db_connection};
 
 #[tokio::main]
 async fn main() {
@@ -25,6 +26,8 @@ async fn main() {
 async fn init() {
     dotenv::dotenv().ok();
     init_git().await;
+    populate_categories().await;
+    populate_challenges().await;
 }
 
 async fn init_git() {
@@ -33,8 +36,39 @@ async fn init_git() {
     clone_or_update_repository(&repo_url, &base_path);
 }
 
+async fn populate_categories() {
+    let base_path = env::var("BASE_PATH").expect("BASE_PATH must be set in the environment");
+    let repo_path = Path::new(&base_path);
+    let mut conn = get_db_connection().await;
+
+    for entry in walkdir::WalkDir::new(repo_path) {
+        let entry = entry.expect("Failed to read directory entry");
+        if entry.file_name() == "metadata.json" {
+            let metadata: serde_json::Value = serde_json::from_str(
+                &std::fs::read_to_string(entry.path()).expect("Failed to read metadata.json")
+            ).expect("Failed to parse metadata.json");
+
+            let category_name = metadata["challenge"]["category"]
+                .as_str()
+                .expect("Category name not found in metadata");
+
+            CategoryService::find_or_create_category(&mut conn, category_name).await.unwrap();
+        }
+    }
+}
+
+async fn populate_challenges() {
+    let base_path = env::var("BASE_PATH").expect("BASE_PATH must be set in the environment");
+    let repo_path = Path::new(&base_path);
+
+    challenge_loader::load_challenges_from_repo(repo_path).await.unwrap();
+}
+
 async fn run_server() {
-    let app = Router::new().merge(routes::user_routes());
+    let app = Router::new()
+        .merge(routes::user_routes())
+        .merge(routes::challenge_routes())
+        .merge(routes::category_routes());
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("Server running on http://0.0.0.0:3000");
