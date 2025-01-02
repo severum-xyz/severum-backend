39c1a7d956a725260966cef83fed84286648ce4a
Antoine James <mushow@proton.me>
2025-01-02 21:38:58 +0100

    refactor(category): Remove insert method from NewCategory

--- src/models/category.rs
+++ src/models/category.rs
@@ -9,20 +9,3 @@ pub struct Category {
 pub struct NewCategory<'a> {
     pub name: &'a str,
 }
-
-impl<'a> NewCategory<'a> {
-    pub async fn insert(&self, pool: &sqlx::PgPool) -> Result<i32, sqlx::Error> {
-        let row: (i32,) = sqlx::query_as(
-            r#"
-            INSERT INTO categories (name)
-            VALUES ($1)
-            RETURNING id
-            "#
-        )
-            .bind(self.name)
-            .fetch_one(pool)
-            .await?;
-
-        Ok(row.0)
-    }
-}
