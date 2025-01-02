873521e255cb3eaa9197f527d0614140aa2a3e50
Antoine James <mushow@proton.me>
2025-01-02 13:59:31 +0100

    refactor(categories): update get_categories handler to use Axum's Extension and async SQLx pool

--- src/controllers/category_controller.rs
+++ src/controllers/category_controller.rs
@@ -1,12 +1,11 @@
-use axum::Json;
+use axum::{Json, Extension};
 use serde::Serialize;
 use log::{info, error};
-
 use crate::{
     services::category_service::CategoryService,
-    utils::get_db_connection,
     controllers::errors::{ControllerError, ErrorResponse},
 };
+use crate::utils::db::DbPool;
 
 #[derive(Serialize)]
 pub struct CategoryResponse {
@@ -14,34 +13,20 @@ pub struct CategoryResponse {
     name: String,
 }
 
-pub async fn get_categories() -> Result<Json<Vec<CategoryResponse>>, ControllerError> {
+pub async fn get_categories(
+    Extension(pool): Extension<DbPool>,
+) -> Result<Json<Vec<CategoryResponse>>, ControllerError> {
     info!("Fetching all categories...");
 
-    let mut conn = get_db_connection().await.unwrap();
-    info!("Database connection established.");
-
-    let categories = tokio::task::spawn_blocking(move || {
-        CategoryService::get_all_categories(&mut conn)
-    })
-        .await
-        .map_err(|e| {
-            error!("Internal server error: {}", e);
-            let error_response = ErrorResponse::new(
-                "INTERNAL_SERVER_ERROR".to_string(),
-                "Failed to fetch categories".to_string(),
-                None,
-            );
-            ControllerError::InternalServerError(error_response)
-        })?
-        .map_err(|e| {
-            error!("Database error: {}", e);
-            let error_response = ErrorResponse::new(
-                "DATABASE_ERROR".to_string(),
-                "Failed to fetch categories".to_string(),
-                None,
-            );
-            ControllerError::InternalServerError(error_response)
-        })?;
+    let categories = CategoryService::get_all_categories(&pool).await.map_err(|e| {
+        error!("Database error: {}", e);
+        let error_response = ErrorResponse::new(
+            "DATABASE_ERROR".to_string(),
+            "Failed to fetch categories".to_string(),
+            None,
+        );
+        ControllerError::InternalServerError(error_response)
+    })?;
 
     let response = categories
         .into_iter()
