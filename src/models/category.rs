8feb4c90da3ef44b3c5ccb3fb13f24fc5cb6b610
Antoine James <mushow@proton.me>
2025-01-02 13:58:57 +0100

    refactor(categories): migrate from Diesel to SQLx for category management

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,16 +1,28 @@
-use crate::schema::categories;
-use diesel::prelude::*;
+use sqlx::FromRow;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = categories)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Debug, FromRow)]
 pub struct Category {
     pub id: i32,
     pub name: String,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = categories)]
 pub struct NewCategory<'a> {
     pub name: &'a str,
+}
+
+impl<'a> NewCategory<'a> {
+    pub async fn insert(&self, pool: &sqlx::PgPool) -> Result<i32, sqlx::Error> {
+        let row: (i32,) = sqlx::query_as(
+            r#"
+            INSERT INTO categories (name)
+            VALUES ($1)
+            RETURNING id
+            "#
+        )
+            .bind(self.name)
+            .fetch_one(pool)
+            .await?;
+
+        Ok(row.0)
+    }
 }
