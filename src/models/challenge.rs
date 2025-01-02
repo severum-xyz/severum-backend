b676bbcb303d2094e76a5d96525bc8a89d06213e
Antoine James <mushow@proton.me>
2025-01-02 14:00:14 +0100

    refactor(challenges): migrate challenge model from Diesel to SQLx

--- src/models/challenge.rs
+++ src/models/challenge.rs
@@ -1,10 +1,7 @@
-use crate::schema::challenges;
+use sqlx::FromRow;
 use chrono::NaiveDateTime;
-use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = challenges)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Debug, FromRow)]
 pub struct Challenge {
     pub id: i32,
     pub category_id: i32,
@@ -15,12 +12,31 @@ pub struct Challenge {
     pub created_at: NaiveDateTime,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = challenges)]
 pub struct NewChallenge<'a> {
     pub category_id: i32,
     pub name: &'a str,
     pub difficulty: &'a str,
     pub description: &'a str,
     pub hint: Option<&'a str>,
+}
+
+impl<'a> NewChallenge<'a> {
+    pub async fn insert(&self, pool: &sqlx::PgPool) -> Result<i32, sqlx::Error> {
+        let row: (i32,) = sqlx::query_as(
+            r#"
+            INSERT INTO challenges (category_id, name, difficulty, description, hint)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id
+            "#
+        )
+            .bind(self.category_id)
+            .bind(self.name)
+            .bind(self.difficulty)
+            .bind(self.description)
+            .bind(self.hint)
+            .fetch_one(pool)
+            .await?;
+
+        Ok(row.0)
+    }
 }
