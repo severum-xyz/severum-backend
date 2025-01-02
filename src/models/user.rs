7bf020f9e0f2d3b8364d525c6befc950a438c0d1
Antoine James <mushow@proton.me>
2025-01-02 14:03:41 +0100

    refactor(users): switch from Diesel to SQLx for user model and insertion

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,10 +1,7 @@
-use crate::schema::users;
+use sqlx::FromRow;
 use chrono::NaiveDateTime;
-use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = users)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Debug, FromRow)]
 pub struct User {
     pub id: i32,
     pub email: String,
@@ -14,10 +11,27 @@ pub struct User {
     pub verified: Option<bool>,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = users)]
 pub struct NewUser<'a> {
     pub email: &'a str,
     pub pseudo: &'a str,
     pub password_hash: &'a str,
+}
+
+impl<'a> NewUser<'a> {
+    pub async fn insert(&self, pool: &sqlx::PgPool) -> Result<i32, sqlx::Error> {
+        let row: (i32,) = sqlx::query_as(
+            r#"
+            INSERT INTO users (email, pseudo, password_hash)
+            VALUES ($1, $2, $3)
+            RETURNING id
+            "#
+        )
+            .bind(self.email)
+            .bind(self.pseudo)
+            .bind(self.password_hash)
+            .fetch_one(pool)
+            .await?;
+
+        Ok(row.0)
+    }
 }
