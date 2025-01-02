6ecc2b7a3baa4187b4bee70e6f5de4fd0740759c
Antoine James <mushow@proton.me>
2025-01-02 23:47:56 +0100

    refactor(k6): simulate load on different stages and durations

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export let options = {
-  vus: 150,
-  duration: '600s',
-  thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(95)<500'],
-  },
+export const options = {
+  stages: [
+    { duration: '30s', target: 500 },
+    { duration: '1m', target: 2000 },
+    { duration: '1m', target: 5000 },
+    { duration: '30s', target: 0 },
+  ],
 };
 
 export default function () {
