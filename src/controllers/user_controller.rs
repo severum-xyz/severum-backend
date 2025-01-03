102e0c35635275c75cc63dca0938545089ce5f7e
Antoine James <mushow@proton.me>
2025-01-03 18:52:13 +0100

    refactor(controllers): optimize user registration and login handlers

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -33,62 +33,68 @@ pub struct LoginResponse {
 
 pub async fn register_user(
     Extension(pool): Extension<DbPool>,
-    Json(payload): Json<RegisterRequest>
+    Json(payload): Json<RegisterRequest>,
 ) -> Result<impl IntoResponse, ControllerError> {
-    match UserService::create_user(&pool, &payload).await {
-        Ok(_) => {
-            info!("User {} registered successfully.", payload.email);
-            Ok(Json(RegisterResponse {
-                message: "User registered successfully.".to_string(),
-            }))
-        }
-        Err(e) => {
-            error!("Error: {}", e);
-            let error_response = match e {
-                RegistrationError::EmailAlreadyTaken => ErrorResponse::new(
-                    "EMAIL_ALREADY_TAKEN".to_string(),
-                    "Email is already taken.".to_string(),
-                    Some("email".to_string()),
-                ),
-                RegistrationError::UsernameAlreadyTaken => ErrorResponse::new(
-                    "USERNAME_ALREADY_TAKEN".to_string(),
-                    "Username is already taken.".to_string(),
-                    Some("pseudo".to_string()),
-                ),
-                _ => ErrorResponse::new(
-                    "INTERNAL_SERVER_ERROR".to_string(),
-                    "Internal server error.".to_string(),
-                    None,
-                ),
-            };
-
-            Err(ControllerError::BadRequest(error_response))
-        }
-    }
+    UserService::create_user(&pool, &payload).await.map(|_| {
+        info!("User {} registered successfully.", payload.email);
+        Json(RegisterResponse {
+            message: "User registered successfully.".to_string(),
+        })
+    })
+        .map_err(|e| {
+            error!("Error registering user: {}", e);
+            ControllerError::BadRequest(map_registration_error(e))
+        })
 }
 
 pub async fn login_user_handler(
     Extension(pool): Extension<DbPool>,
-    Json(payload): Json<LoginRequest>
+    Json(payload): Json<LoginRequest>,
 ) -> Result<Json<LoginResponse>, ControllerError> {
-    let token = UserService::login_user(&pool, &payload).await.map_err(|e| {
-        let error_response = match e {
-            LoginError::InvalidCredentials => ErrorResponse::new(
-                "INVALID_CREDENTIALS".to_string(),
-                "Email or password is incorrect.".to_string(),
-                Some("email".to_string()),
-            ),
-            _ => ErrorResponse::new(
-                "INTERNAL_SERVER_ERROR".to_string(),
-                "Internal server error.".to_string(),
-                None,
-            ),
-        };
-        ControllerError::BadRequest(error_response)
-    })?;
+    UserService::login_user(&pool, &payload).await.map(|token| {
+        info!("User {} logged in successfully.", payload.email);
+        Json(LoginResponse {
+            message: "User logged in successfully.".to_string(),
+            token,
+        })
+    })
+        .map_err(|e| {
+            error!("Error logging in user: {}", e);
+            ControllerError::BadRequest(map_login_error(e))
+        })
+}
 
-    Ok(Json(LoginResponse {
-        message: "User logged in successfully.".to_string(),
-        token,
-    }))
+fn map_registration_error(e: RegistrationError) -> ErrorResponse {
+    match e {
+        RegistrationError::EmailAlreadyTaken => ErrorResponse::new(
+            "EMAIL_ALREADY_TAKEN".to_string(),
+            "Email is already taken.".to_string(),
+            Some("email".to_string()),
+        ),
+        RegistrationError::UsernameAlreadyTaken => ErrorResponse::new(
+            "USERNAME_ALREADY_TAKEN".to_string(),
+            "Username is already taken.".to_string(),
+            Some("pseudo".to_string()),
+        ),
+        _ => ErrorResponse::new(
+            "INTERNAL_SERVER_ERROR".to_string(),
+            "Internal server error.".to_string(),
+            None,
+        ),
+    }
+}
+
+fn map_login_error(e: LoginError) -> ErrorResponse {
+    match e {
+        LoginError::InvalidCredentials => ErrorResponse::new(
+            "INVALID_CREDENTIALS".to_string(),
+            "Email or password is incorrect.".to_string(),
+            Some("email".to_string()),
+        ),
+        _ => ErrorResponse::new(
+            "INTERNAL_SERVER_ERROR".to_string(),
+            "Internal server error.".to_string(),
+            None,
+        ),
+    }
 }
