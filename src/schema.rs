9d6a938307177b85a43b904e14298c02e4c2b4fb
Antoine James <mushow@proton.me>
2025-01-05 01:25:04 +0100

    feat(schema): update `schema.rs` using the latest migration

--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,21 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    _sqlx_migrations (version) {
+        version -> Int8,
+        description -> Text,
+        installed_on -> Timestamptz,
+        success -> Bool,
+        checksum -> Bytea,
+        execution_time -> Int8,
+    }
+}
+
+diesel::table! {
     categories (id) {
         id -> Int4,
         #[max_length = 255]
-        name -> Varchar
+        name -> Varchar,
     }
 }
 
@@ -23,6 +34,23 @@ diesel::table! {
 }
 
 diesel::table! {
+    roles (id) {
+        id -> Int4,
+        #[max_length = 50]
+        name -> Varchar,
+    }
+}
+
+diesel::table! {
+    user_containers (id) {
+        id -> Int4,
+        user_id -> Int4,
+        container_name -> Uuid,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         #[max_length = 255]
@@ -33,13 +61,19 @@ diesel::table! {
         password_hash -> Varchar,
         created_at -> Timestamp,
         verified -> Nullable<Bool>,
+        role_id -> Int4,
     }
 }
 
 diesel::joinable!(challenges -> categories (category_id));
+diesel::joinable!(user_containers -> users (user_id));
+diesel::joinable!(users -> roles (role_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    _sqlx_migrations,
     categories,
     challenges,
+    roles,
+    user_containers,
     users,
 );
