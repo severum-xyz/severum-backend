f6a1600de5e24e3a3f9d400cc7576db843ffbb0a
Antoine James <mushow@proton.me>
2025-01-13 14:40:36 +0100

    docs(container): added code documentation

--- src/models/container.rs
+++ src/models/container.rs
@@ -3,6 +3,10 @@ use serde::Serialize;
 use sqlx::FromRow;
 use uuid::Uuid;
 
+/// Represents a user's container in the system.
+///
+/// This struct maps to the `user_containers` table and includes details
+/// such as the user ID, challenge, and category associated with the container.
 #[derive(Debug, FromRow, Serialize)]
 pub struct UserContainer {
     pub id: i32,
@@ -13,6 +17,9 @@ pub struct UserContainer {
     pub created_at: NaiveDateTime,
 }
 
+/// Represents a new container to be created in the system.
+///
+/// Used for inserting container details into the database.
 pub struct NewContainer {
     pub user_id: i32,
     pub challenge_id: i32,
