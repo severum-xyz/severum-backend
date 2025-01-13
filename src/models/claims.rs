9978c9d4858368c25679ba2654222e7991ccc501
Antoine James <mushow@proton.me>
2025-01-13 14:40:21 +0100

    docs(jwt-claims): added code documentation

--- src/models/claims.rs
+++ src/models/claims.rs
@@ -1,7 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// Represents the claims used in JWT tokens.
+///
+/// This struct includes the subject (user identifier) and token expiration time.
 #[derive(Clone, Deserialize, Serialize)]
 pub struct Claims {
+    /// The subject of the token, typically the user ID.
     pub sub: String,
+
+    /// The expiration time of the token, represented as a Unix timestamp.
     pub exp: usize,
 }
