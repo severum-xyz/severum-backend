ce93a9136c9420129b9260b477fbcc72e940fb4e
Antoine James <mushow@proton.me>
2025-01-13 11:40:02 +0100

    docs(docker): added code documentation

--- src/utils/docker.rs
+++ src/utils/docker.rs
@@ -1,5 +1,10 @@
 use std::env;
 
+/// Retrieves the Docker image name from the `DOCKER_IMAGE` environment variable.
+///
+/// # Returns
+/// A `String` containing the Docker image name. Defaults to `"0xmushow/severum:severum-sandbox-0.0.1"`
+/// if the `DOCKER_IMAGE` variable is not set.
 pub fn get_docker_image() -> String {
     env::var("DOCKER_IMAGE").unwrap_or_else(|_| "0xmushow/severum:severum-sandbox-0.0.1".to_string())
 }
