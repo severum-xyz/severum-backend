5d0929111a661ea10a18bd2dc5a31279196d9d60
Antoine James <mushow@proton.me>
2025-01-13 14:40:15 +0100

    docs(challenge): added code documentation

--- src/models/challenge.rs
+++ src/models/challenge.rs
@@ -2,6 +2,10 @@ use sqlx::FromRow;
 use chrono::NaiveDateTime;
 use serde::Serialize;
 
+/// Represents a challenge in the system.
+///
+/// This struct maps to the `challenges` table and includes details such as
+/// the category, difficulty, and description of a challenge.
 #[derive(Serialize, Debug, FromRow)]
 pub struct Challenge {
     pub id: i32,
@@ -13,6 +17,9 @@ pub struct Challenge {
     pub created_at: NaiveDateTime,
 }
 
+/// Represents a new challenge to be created in the system.
+///
+/// Used for inserting challenge details into the database.
 pub struct NewChallenge {
     pub category_id: i32,
     pub name: String,
