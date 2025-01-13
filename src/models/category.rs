026c2d827dc48dcca74afbf051675888e4f7b2df
Antoine James <mushow@proton.me>
2025-01-13 14:40:12 +0100

    docs(category): added code documentation

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,11 +1,17 @@
 use sqlx::FromRow;
 
+/// Represents a category in the system.
+///
+/// This struct maps to the `categories` table and stores details about a category.
 #[derive(Debug, FromRow)]
 pub struct Category {
     pub id: i32,
     pub name: String,
 }
 
+/// Represents a new category to be added to the system.
+///
+/// Used for inserting a new category into the database.
 pub struct NewCategory {
     pub name: String,
 }
